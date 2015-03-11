bdd87bae0f0e353342cead2f384576b474f295e5
riven-m <lrw90316@163.com>
2015-03-11 09:24:47 +0800

    Initial Commit

---
+++ Smartlock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Smartlock
+//
+//  Created by RivenL on 15/3/11.
+//  Copyright (c) 2015年 RivenL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Smartlock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Smartlock
+//
+//  Created by RivenL on 15/3/11.
+//  Copyright (c) 2015年 RivenL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
